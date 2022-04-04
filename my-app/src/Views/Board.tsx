167cc60da194ec93be6cdc6d5c1d77f5aa235c5d
paulstumpe <paul.andrew.stumpe@gmail.com>
2022-04-03 22:53:28 -0500

    hexes working

---
+++ my-app/src/Views/Board.tsx
@@ -0,0 +1,16 @@
+import HexagonBoard from "./HexagonBoard";
+type props = {
+
+}
+
+function Board({}:props) {
+
+    return (
+        <div>
+            <HexagonBoard/>
+        </div>
+
+    );
+}
+
+export default Board;
